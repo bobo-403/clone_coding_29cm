64c4651cecec51c90d44475f904b9ed9c6f5ffa8
bobo-403 <139124147+bobo-403@users.noreply.github.com>
2024-08-29 22:10:36 +0900

    물품정보 슬라이드 페이지 번호 추가

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-var slideCount = ducument.querySelectorAll('.slide img').length;
-var currnetIdx = 0;
+var currentIdx = 1;
+var slideWidth = 330;
+
+
+function getSlideCount() {
+    return document.querySelectorAll('.slide_flex img').length;
+}
+
+function initializeSlides() {
+    totalSlides = getSlideCount(); 
+    document.getElementById('currentSlide').innerText = currentIdx;
+    document.getElementById('totalSlides').innerText = totalSlides;
+}
+
+function updateSlideInfo(){
+    document.getElementById('currentSlide').innerText = currentIdx;
+    document.getElementById('totalSlides').innerText = getSlideCount();
+}
+
+function nextslide(){
+    var slideCount = getSlideCount();
+    if(currentIdx < slideCount){
+        document.querySelector('.slide_flex').style.left = (-slideWidth*currentIdx)+'px';
+        currentIdx ++;
+    }
+    updateSlideInfo();
+    
+}
+            
+function prevslide(){
+    if(currentIdx > 1){
+        currentIdx=currentIdx-1;
+        document.querySelector('.slide_flex').style.left = (-slideWidth*(currentIdx-1))+'px';
+    }
+    updateSlideInfo();
+    
+}
+
+document.addEventListener('DOMContentLoaded', initializeSlides);
